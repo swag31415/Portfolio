57af1a2c9866a15b5d22845e225b2580e4b55dde
swag31415 <kidswagatam18@gmail.com>
2021-12-16 19:39:08 -0500

    Added inserts and made the base page

---
+++ js/html_inserts.js
@@ -0,0 +1,19 @@
+customElements.define('insert-html', class InsertHTML extends HTMLElement {
+  constructor () {
+    super()
+    let link = this.getAttribute('link')
+    fetch(link).then(resp => {
+      if (!resp.ok) throw new Error(resp.statusText)
+      return resp.text()
+    })
+    .then(html => {
+      let content = document.createRange().createContextualFragment(html)
+      this.after(content)
+    })
+    .catch(err => {
+      console.error(`Failed to insert HTML '${link}'`)
+      console.error(err)
+    })
+    .finally(() => this.remove())
+  }
+})
