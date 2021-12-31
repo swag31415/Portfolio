89eca9b27f53067d60f292b1b8a6910fac7e20b3
swag31415 <kidswagatam18@gmail.com>
2021-12-30 19:34:23 -0500

    Added all the useful things as a Firebase object to the window

--- js/firebase.js
+++ js/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
-import { getFirestore, collection, doc, addDoc, getDoc, getDocs, setDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { getFirestore, collection, doc, addDoc, getDoc, getDocs, setDoc, deleteDoc, updateDoc, query, where, orderBy, limit, startAt, endAt } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 import { getAnalytics, logEvent } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-analytics.js";
 
 // Your web app's Firebase configuration
@@ -21,3 +21,51 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const db = getFirestore();
 const analytics = getAnalytics(app);
+
+// Export the good stuff
+window.Firebase = {
+  auth: {
+    login: function (email, password) {
+      return signInWithEmailAndPassword(auth, email, password)
+    },
+    logout: function () {
+      return auth.signOut()
+    },
+    set_on_auth_change: function (func) {
+      return onAuthStateChanged(auth, func)
+    }
+  },
+  firestore: {
+    add: function (collection_name, data, doc_id) {
+      if (doc_id) return setDoc(doc(db, collection, doc_id), data)
+      else return addDoc(collection(db, collection_name), data)
+    },
+    update: function (collection_name, doc_id, data) {
+      return updateDoc(doc(db, collection_name, doc_id), data)
+    },
+    drop: function (collection_name, doc_id) {
+      return deleteDoc(doc(db, collection_name, doc_id))
+    },
+    get: function (collection_name, doc_id, conditions, orders, count, start, end) {
+      if (doc_id) return getDoc(doc(db, collection_name, doc_id)).then(function (snap) {
+        if (snap.exists()) return snap
+        else throw Error('No such document')
+      })
+      else {
+        if (conditions || orders || count || start || end) {
+          let params = [...conditions.map(cond => where(...cond)), ...orders.map(ord => orderBy(...ord))]
+          if (count) params.push(limit(count))
+          if (start) params.push(startAt(start))
+          if (end) params.push(endAt(end))
+          return getDocs(query(collection(db, collection_name), ...params)).then(q => q.docs)
+        }
+        else return getDocs(collection(db, collection_name)).then(q => q.docs)
+      }
+    }
+  },
+  analytics: {
+    log: function (event, params) {
+      return logEvent(analytics, event, params)
+    }
+  }
+}
